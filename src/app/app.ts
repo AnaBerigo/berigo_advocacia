ecfb5c48ade367279f1e9d3826ed3c0e4916fac9
Ana Berigo <ana.berigo17@gmail.com>
2025-12-30 16:39:16 -0300

    feat: Corrige estilos e responsividade do CTA e ajusta comportamento e espaçamento da navbar fixa.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cta } from './components/cta/cta';
 import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Cta],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
